219728a57753b8c53dc1a352c605e94973cfd5bf
ahmetaric <ahmet.aric1965@gmail.com>
2022-10-06 14:04:07 +0300

    v-4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import Contacts from './components/contacts/Contacts';
 import FormComponent from './components/form/FormComponent';
+import { AddUser } from './utils/functions';
 // import FormComponent from './components/form/FormComponent';
 
 const initialValues = {username:"",phoneNumber:"",gender:""}
@@ -10,8 +11,7 @@ function App() {
   const [info, setInfo] = useState(initialValues);
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(info);
-    // AddUser();
+    AddUser(info);
   }
   return (
     <div className="App">
